57c0c987657e42c1c09255a294b613a28d6a4b8a
Rodion.swift <Rodion.swift@gmail.com>
2019-11-30 23:33:25 +0300

    2

--- UIWebView+UIActivityIndicator+openPDF/ViewController.swift
+++ UIWebView+UIActivityIndicator+openPDF/ViewController.swift
@@ -17,8 +17,6 @@ class ViewController: UIViewController, UIWebViewDelegate {
     
     let application = UIApplication.shared
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
